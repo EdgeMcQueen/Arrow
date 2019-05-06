11a4e8d5f72bc62f6add21b4fb30df517003e455
Sardor.Mutalov <45397981+SardorMutalov@users.noreply.github.com>
2019-05-06 20:39:34 +0300

    бургер меню

---
+++ src/js/list-carousel.js
@@ -0,0 +1,11 @@
+// Вертикальная карусель
+
+$('burger-menu').slick({
+infinite: false,
+vertical:true,
+verticalSwiping:true,
+slidesToShow: 3,
+slidesToScroll: 3,
+//prevArrow: $('.top-arrow'),
+//nextArrow: $('.bottom-arrow')
+});
