6d923581152d25b12ac7250e94dcfb724fe0f0c7
NИ <Ko2dooDev@gmail.com>
2019-06-24 09:42:48 +0500

    update

---
+++ app/js/wrapSelector.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	let wrapSelector 	= '<div class="table-responsive"></div>';
+	$('.team-box').addClass('table').wrap(wrapSelector);
+		
+		// function wrappAppend () {
+		// 	if ( $('div').is('.table-responsive') ) {
+		// 		$('.team-box').addClass('table');
+		// 	}
+		// }
+
+		// wrappAppend ();
+
+});
