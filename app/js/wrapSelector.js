20ca9d99507e18dfc30d85e7ffd84de791b7257d
NИ <Ko2dooDev@gmail.com>
2019-06-24 17:30:11 +0500

    update?

--- app/js/wrapSelector.js
+++ app/js/wrapSelector.js
@@ -3,6 +3,13 @@ jQuery(document).ready(function($) {
 	let wrapSelector 	= '<div class="table-responsive"></div>';
 	$('.team-box').addClass('table').wrap(wrapSelector);
 		
+
+		// получим элемент и обернем его содержимое классом .table-responsive
+		// из переменной wrapSelector
+		// $('.team-box').wrap(wrapSelector);
+
+		// // ниже запишем функцию с проверкой:
+
 		// function wrappAppend () {
 		// 	if ( $('div').is('.table-responsive') ) {
 		// 		$('.team-box').addClass('table');
@@ -14,8 +21,6 @@ jQuery(document).ready(function($) {
 		/*
 			! - Скрипт по оборачиванию по селектору
 				~ сначала получаем элемент, $('') потом присваиваем к нему класс и методом wrap() оборачиваем все это тем что находится в переменной в 3-ей строке
-
-				с 6 строки начинается второй вариант с проверкой, менее изящный но тоже допустимый.
 
 		*/
 
